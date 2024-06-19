889fc8f4cee9acfec2ddab29c85da219e597e723
WardonNE <1730314864@qq.com>
2024-06-19 14:48:49 +0800

    update

--- go.mod
+++ go.mod
@@ -2,15 +2,12 @@ module github.com/gopi-frame/config
 
 go 1.22.2
 
-require (
-	github.com/gopi-frame/contract v0.0.0-20240517053949-c2988e7dcb26
-	github.com/gopi-frame/support v0.0.0-20240517030649-e39b6da6876b
-	github.com/spf13/viper v1.18.2
-)
+require github.com/spf13/viper v1.18.2
+
+require github.com/stretchr/testify v1.9.0 // indirect
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
-	github.com/gopi-frame/exception v0.0.0-20240517053912-ba20d5865bca
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -22,10 +19,9 @@ require (
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- repository.go
+++ repository.go
@@ -1,92 +1,49 @@
 package config
 
 import (
-	"io"
-
-	"github.com/gopi-frame/support/maps"
 	"github.com/spf13/viper"
 )
 
-// NewRepository new repository
 func NewRepository() *Repository {
-	r := new(Repository)
-	r.modules = maps.NewMap[string, *viper.Viper]()
-	return r
+	repo := new(Repository)
+	repo.Viper = viper.New()
+	return repo
 }
 
 // Repository repository
 type Repository struct {
-	modules *maps.Map[string, *viper.Viper]
+	*viper.Viper
 }
 
-// Module get module
-func (repo *Repository) Module(module string) *viper.Viper {
-	if repo.modules.TryLock() {
-		defer repo.modules.Unlock()
-	}
-	return repo.modules.GetOr(module, nil)
-}
-
-func (repo *Repository) Read(module string, reader io.Reader) error {
-	if repo.modules.TryLock() {
-		defer repo.modules.Unlock()
-	}
-	viper := viper.New()
-	err := viper.ReadConfig(reader)
-	if err != nil {
-		return err
-	}
-	repo.modules.Set(module, viper)
-	return nil
-}
-
-// Set set
-func (repo *Repository) Set(module string, key string, value any) {
-	if repo.modules.TryLock() {
-		defer repo.modules.Unlock()
-	}
-	repo.Module(module).Set(key, value)
+func (repo *Repository) All() map[string]any {
+	return repo.Viper.AllSettings()
 }
 
 // Has has
-func (repo *Repository) Has(module string, key string) bool {
-	if repo.modules.TryLock() {
-		defer repo.modules.Unlock()
-	}
-	if module := repo.Module(module); module != nil {
-		return module.IsSet(key)
-	}
-	return false
+func (repo *Repository) Has(path string) bool {
+	return repo.IsSet(path)
 }
 
 // Get get
-func (repo *Repository) Get(module string, key string, defaultValue ...any) any {
-	if repo.modules.TryLock() {
-		defer repo.modules.Unlock()
+func (repo *Repository) Get(path string, defaultValue ...any) any {
+	if repo.Has(path) {
+		return repo.Viper.Get(path)
 	}
-	if !repo.Has(module, key) {
-		if len(defaultValue) > 0 {
-			return defaultValue[0]
-		}
-		return nil
+	if len(defaultValue) > 0 {
+		return defaultValue[0]
 	}
-	return repo.Module(module).Get(key)
+	return nil
+}
+
+func (repo *Repository) Set(path string, value any) {
+	repo.Viper.Set(path, value)
 }
 
 // Unmarshal unmarshal
-func (repo *Repository) Unmarshal(module string, dest any) error {
-	viper, ok := repo.modules.Get(module)
-	if !ok {
-		return NewModuleNotFoundException(module)
-	}
-	return viper.Unmarshal(dest)
+func (repo *Repository) Unmarshal(dest any) error {
+	return repo.Viper.Unmarshal(dest)
 }
 
-// UnmarshalKey unmarshal key
-func (repo *Repository) UnmarshalKey(module string, key string, dest any) error {
-	viper, ok := repo.modules.Get(module)
-	if !ok {
-		return NewModuleNotFoundException(module)
-	}
-	return viper.UnmarshalKey(key, dest)
+func (repo *Repository) UnmarshalKey(key string, dest any) error {
+	return repo.Viper.UnmarshalKey(key, dest)
 }
